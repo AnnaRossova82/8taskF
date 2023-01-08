33decbdfe27be312fc99bf33d7b3beb0b1703381
AnnaRossova82 <rossova.anna@gmail.com>
2023-01-08 12:32:00 +0200

    Third commit for py_charm some new text

--- py_charm.py
+++ py_charm.py
@@ -1,3 +1,3 @@
 print ("Hello from new py_charm file")
 print ("Add some text for new commit from new py_charm file")
-print ("add text to make third commit for py_charm file")
+print ("Add text to make third commit for py_charm file")
