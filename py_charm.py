a87e930fedb517cb330484edefe2e24cb989aaf9
AnnaRossova82 <rossova.anna@gmail.com>
2023-01-08 12:52:38 +0200

    Commit 4 py_charm merge test maybe

--- py_charm.py
+++ py_charm.py
@@ -1,3 +1,4 @@
 print ("Hello from new py_charm file")
 print ("Add some text for new commit from new py_charm file")
 print ("Add text to make third commit for py_charm file")
+print ("Now merge test into master maybe...")
