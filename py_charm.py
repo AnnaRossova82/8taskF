fb29a271a33dbe7485f2fd8ffb1aee199609edef
AnnaRossova82 <rossova.anna@gmail.com>
2023-01-08 14:33:31 +0200

    Commit 7 py_charm add charm_func for confl creat

--- py_charm.py
+++ py_charm.py
@@ -2,3 +2,6 @@ print ("Hello from new py_charm file")
 print ("Add some text for new commit from new py_charm file")
 print ("Add text to make third commit for py_charm file")
 print ("Now merge test into master maybe...")
+def charm_func (q, w):
+    return q + w
+print (charm_func (1, 3))
